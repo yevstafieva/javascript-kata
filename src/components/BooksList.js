7d38381f513a17c8de2d31256bfac6d70ca75191
Tamara Y <t.yevstafieva@gmail.com>
2020-04-25 22:09:30 +0200

    Books authors

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 
-const BooksList = ({books}) => {
-    
-        if (books?.books){
-            return (<ListGroup>
-                {books.books.map(book => <ListGroupItem key={book.isbn}>{book.title} by <b>Author</b> (ISBN: {book.isbn}) </ListGroupItem> )}
-            </ListGroup>)
-        } else {
-            return <p>There were no books loaded</p>
-        }
+const BooksList = ({books, authors}) => {
+    const lookUpAurhors = (book) => {
+       return (book.authors.split(",").map(author => `${authors.authors[author].firstname} ${authors.authors[author].lastname}`).join(', '))
+    }
+    if (books?.books && authors?.authors ){
+        return (<ListGroup>
+            {books.books.map(book => <ListGroupItem key={book.isbn}>{book.title} <br /> by <b>{lookUpAurhors(book)}</b> <br /> (ISBN: {book.isbn}) </ListGroupItem> )}
+        </ListGroup>)
+    } else {
+        return <p>There were no books loaded</p>
+    }
     
 }
 
